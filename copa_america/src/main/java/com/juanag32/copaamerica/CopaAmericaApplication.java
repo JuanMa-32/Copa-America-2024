08dbbc5ce41567e36befcbf916b335a92e86b746
Juan Manuel Agüero <juanmaguero4@gmail.com>
2024-06-06 19:13:35 -0300

    estructura de carpetas

---
+++ copa_america/src/main/java/com/juanag32/copaamerica/CopaAmericaApplication.java
@@ -0,0 +1,13 @@
+package com.juanag32.copaamerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopaAmericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopaAmericaApplication.class, args);
+	}
+
+}
